d2741ae7e1141b8bc3d4126de4510367ff1ad973
akrchnv <52063796+akrchnv@users.noreply.github.com>
2023-03-05 00:51:48 +0300

    3. added code

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введи что попало через пробел: ");
+string[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int res = ResArrLength(arr);
+Console.Write($"[{String.Join(", ", FillArray(res))}]");
+
+int ResArrLength(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] FillArray(int count)
+{
+    string[] array = new string[count];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            array[j] = arr[i];
+            j++;
+        }
+    }
+    return array;
+}
